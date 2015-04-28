3d44524f5c48cd3d418e9dea6e85e01f6981a2b3
Drew & Ian <student@epicodus.com>
2015-04-28 10:15:58 -0700

    add method logic to save location to list, passing save_location unit test

--- lib/places.rb
+++ lib/places.rb
@@ -1,5 +1,7 @@
 class Places
 
+  @@all_locations = []
+
   define_method(:initialize) do |location_name|
     @location_name = location_name
   end
@@ -9,10 +11,10 @@ class Places
   end
 
   define_method(:save_location) do
-
+    @@all_locations.push(self)
   end
 
   define_singleton_method(:all) do
-
+    @@all_locations
   end
 end
